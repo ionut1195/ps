78e64c359f0cea44f7afe9888f187331dcd14bf8
Ionut Tomescu <itomescu@c2s15d81.42wolfsburg.de>
2021-12-27 14:58:40 +0100

    complete

--- sort_arr.c
+++ sort_arr.c
@@ -3,31 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   sort_arr.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: itomescu <itomescu@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: itomescu <itomescu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 20:51:40 by itomescu          #+#    #+#             */
-/*   Updated: 2021/11/19 08:49:31 by itomescu         ###   ########.fr       */
+/*   Updated: 2021/12/27 14:57:39 by itomescu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	increment_s(long *ar, long *s, long pivot)
+{
+	while (ar[*s] < pivot)
+		(*s)++;
+}
+void	decrement_e(long *ar, long *e, long pivot)
+{
+	while (ar[*e] > pivot)
+		(*e)--;
+}
 
 void	sort_arr(long arr[], long low, long hi)
 {
 	long	s;
 	long	e;
 	long	temp;
+	long	pivot;
 
 	if (low > hi)
 		return ;
 	s = low;
 	e = hi;
+	pivot = arr[s + (e - s) / 2];
 	while (s <= e)
 	{
-		while (arr[s] < arr[s + (e - s) / 2])
-			s++;
-		while (arr[e] > arr[s + (e - s) / 2])
-			e--;
+		increment_s(arr, &s, pivot);
+		decrement_e(arr, &e, pivot);
 		if (s <= e)
 		{
 			temp = arr[s];
